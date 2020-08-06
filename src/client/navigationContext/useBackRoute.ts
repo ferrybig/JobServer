fb7343df1bd800a64c26b4141dee02f605a6cfc6
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-06 21:09:37 +0200

    Accept null values

--- src/client/navigationContext/useBackRoute.ts
+++ src/client/navigationContext/useBackRoute.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { RouteDefinication } from '../components/minirouter/route';
 import useBack from './useBack';
 
-export default function useBackRoute<R>(route: RouteDefinication<any, R>, props: R) {
-	const back = useMemo(() => route.toPath(props), [route, props]);
+export default function useBackRoute<R>(route: RouteDefinication<any, R> | null, props: R) {
+	const back = useMemo(() => route ? route.toPath(props) : null, [route, props]);
 	useBack(back);
 }
