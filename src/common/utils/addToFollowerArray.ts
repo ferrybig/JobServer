7f5defab01ff70446f8673c53dd1304fbc7a29cd
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-06 20:24:04 +0200

    Introduce addToFollowerArray utility

---
+++ src/common/utils/addToFollowerArray.ts
@@ -0,0 +1,15 @@
+interface SimpleArray<T> {
+	push(item: T): number;
+	indexOf(item: T): number;
+	splice(index: number, count: 1): T[],
+}
+
+export function addToFollowerArray<F extends (...args: any[]) => any>(array: SimpleArray<F>, newElement: F): () => void {
+	array.push(newElement);
+	return () => {
+		const index = array.indexOf(newElement);
+		if (index >= 0) {
+			array.splice(index, 1);
+		}
+	};
+}
