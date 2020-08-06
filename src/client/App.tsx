d29875aa1cbc95dc72980aa81e2aa3053b466335
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-06 20:23:08 +0200

    Introduce root css to the app

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,34 @@
-import React, { FC } from 'react';
-//import classes from './App.module.css';
+import React, { FC, useLayoutEffect } from 'react';
+import classes from './App.module.css';
 import * as routes from './routes';
 import makeRouter from './components/minirouter/makeRouter';
+import TopBar from './components/TopBar';
+import NavigationContextProvider from './navigationContext';
+import RootThemeInjector from './themesContext/RootThemeInjector';
+import ThemeContextProdivder from './themesContext';
 
 const Router = makeRouter(Object.values(routes));
 
 const App: FC = (): JSX.Element => {
+	useLayoutEffect(() => {
+		document.body.classList.add(classes.body);
+		document.body.parentElement!.classList.add(classes.html);
+		return () => {
+			document.body.classList.remove(classes.body);
+			document.body.parentElement!.classList.remove(classes.html);
+		};
+	}, []);
+
 	return (
-		<Router/>
+		<NavigationContextProvider>
+			<ThemeContextProdivder>
+				<RootThemeInjector/>
+				<div className={classes.root}>
+					<TopBar/>
+					<Router/>
+				</div>
+			</ThemeContextProdivder>
+		</NavigationContextProvider>
 	);
 };
 
