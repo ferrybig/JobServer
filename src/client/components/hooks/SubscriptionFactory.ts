d8324950709633903f6c8425560dc9a0a4726c5a
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-06 21:10:22 +0200

    Add subscription factory

---
+++ src/client/components/hooks/SubscriptionFactory.ts
@@ -0,0 +1,12 @@
+import { useState, useLayoutEffect } from 'react';
+
+export default function subscriptionFactory<C, V>(getValue: (context: C) => V, subscribe: (context: C, handler: () => void) => () => void, name: string = 'useSubscription'): (context: C) => V {
+	function useSubscription(context: C) {
+		const [state, setState] = useState<V>(getValue(context));
+		useLayoutEffect(() => subscribe(context, () => {
+			setState(getValue(context));
+		}), [context, setState]);
+		return state;
+	};
+	return useSubscription;
+}
