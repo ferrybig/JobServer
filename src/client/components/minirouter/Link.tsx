224afe8286da9128d0316537f318e034c965b3b0
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-06 21:07:51 +0200

    Split Link into Link & RouteLink

--- src/client/components/minirouter/Link.tsx
+++ src/client/components/minirouter/Link.tsx
@@ -1,15 +1,12 @@
 import React, { ReactNode, ReactHTML, HTMLAttributes, useCallback } from 'react';
 import { contextLocation } from './LocationService';
-import { RouteDefinication } from './route';
 
-export default function Link<R>({ route, props, children, tag, onClick, ...rest }: {
-	route: RouteDefinication<any, R>,
-	props: R,
+export default function Link({ path, children, tag, onClick, ...rest }: {
+	path: string,
 	children: ReactNode,
 	tag?: keyof ReactHTML,
 } & HTMLAttributes<HTMLElement>): JSX.Element | null {
 	const [update, format] = contextLocation.useUpdate();
-	const path = route.toPath(props);
 	const realTag = tag || 'a' as const;
 	const realOnClick = useCallback((e: React.MouseEvent<HTMLElement, MouseEvent>) => {
 		if (onClick) {
@@ -26,4 +23,3 @@ export default function Link<R>({ route, props, children, tag, onClick, ...rest 
 		...rest,
 	}, children);
 }
-
---
+++ src/client/components/minirouter/RouteLink.tsx
@@ -0,0 +1,14 @@
+import React, { ComponentProps } from 'react';
+import { RouteDefinication } from './route';
+import Link from './Link';
+
+export default function RouteLink<R>({ route, props, ...rest }: {
+	route: RouteDefinication<any, R>,
+	props: R,
+} & Omit<ComponentProps<typeof Link>, 'path'>): JSX.Element | null {
+	const path = route.toPath(props);
+	return (
+		<Link path={path} {...rest}/>
+	);
+}
+
