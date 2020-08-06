fa7db7c79172041e594645863c3084cefe3abcd5
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-06 22:49:40 +0200

    Make an useOptionalView for when you don't have your arguments yet

--- src/client/views/useView.ts
+++ src/client/views/useView.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-type UseResult<D, R> = {
+export type UseView<D, R> = {
 	status: 'loading',
 	data: R,
 } | {
@@ -11,12 +11,49 @@ type UseResult<D, R> = {
 	data: R,
 };
 
+type MapArguments<A extends string[]> =
+	A extends [] ? [] :
+	A extends [string] ? [string | null] :
+	A extends [string, string] ? [string | null, string | null] :
+	A extends [string, string, string] ? [string | null, string | null, string | null] :
+	A extends [string, string, string, string] ? [string | null, string | null, string | null, string | null] :
+	(string | null)[];
+
+export function useOptionalView<A extends string[], D, R>(
+	view: (handler: (data: D | null) => void, ...options: A) => () => void,
+	defaultValue: R,
+	...options: MapArguments<A>
+): UseView<D, R> {
+	const [value, setValue] = useState<UseView<D, R>>({
+		status: 'loading',
+		data: defaultValue,
+	});
+	useEffect(() => {
+		if (options.indexOf(null)) {
+			return;
+		}
+		return view((data: D | null) => {
+			if (data === null) {
+				setValue((oldState) => oldState.status === 'loading' ? { ...oldState, status: 'not-found' } : oldState);
+			} else {
+				setValue({
+					status: 'success',
+					data,
+				});
+			}
+		}, ...options as unknown as A);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [view, setValue, ...options]); // TODO use JSON.stringify here if we want to support length changing of the options list
+	return value;
+}
+
+
 export default function useView<A extends string[], D, R>(
 	view: (handler: (data: D | null) => void, ...options: A) => () => void,
 	defaultValue: R,
 	...options: A
-): UseResult<D, R> {
-	const [value, setValue] = useState<UseResult<D, R>>({
+): UseView<D, R> {
+	const [value, setValue] = useState<UseView<D, R>>({
 		status: 'loading',
 		data: defaultValue,
 	});
