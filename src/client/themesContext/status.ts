e262b46999289b0c592a04ca784defe0d0a3757f
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-07 17:59:51 +0200

    Style fixes

--- src/client/themesContext/status.ts
+++ src/client/themesContext/status.ts
@@ -1,4 +1,4 @@
-import {Task, Deployment, PlatformTask} from "../../common/types";
+import { Task, Deployment, PlatformTask } from '../../common/types';
 import classes from './statusModule/status.module.css';
 
 const statusClasses: Record<Task['status'] | Deployment['status'] | PlatformTask['status'], string> = {
@@ -11,11 +11,11 @@ const statusClasses: Record<Task['status'] | Deployment['status'] | PlatformTask
 	timeout: classes.timeout,
 	cancelled: classes.cancelled,
 	pending: classes.pending,
-}
+};
 if (process.env.NODE_ENV === 'development') {
-	for(const [key, value] of Object.entries(statusClasses)) {
-		if(!value) {
-			console.error('statusClasses[' + key + '] == falsy')
+	for (const [key, value] of Object.entries(statusClasses)) {
+		if (!value) {
+			console.error('statusClasses[' + key + '] == falsy');
 		}
 	}
 }
