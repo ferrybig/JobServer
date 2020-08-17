b9f907760724d6d186a95f026c993625dd1703fc
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-17 10:12:16 +0200

    Properly check things when uploading, and don't truncate the already written bytes

--- src/server/pages/upload.ts
+++ src/server/pages/upload.ts
@@ -1,28 +1,48 @@
 import { Request, Response, NextFunction } from 'express';
+import { FileHandle } from 'fs/promises';
+import { dirname } from 'path';
 import store from '../store/';
 import { getOrNull } from '../store/selectors';
-import { dirname } from 'path';
 import { mkdir, open } from '../../common/async/fs';
 import { crudDelete } from '../store/actions';
 
+async function openFile(path: string, offset: number): Promise<FileHandle> {
+	const dir = dirname(path);
+	await mkdir(dir, { recursive: true });
+	const fd = await open(path, 'a');
+	try {
+		const stat = await fd.stat();
+		if (stat.size < offset) {
+			throw new Error('File is smaller than the requested offset');
+		}
+	} catch(e) {
+		fd.close();
+		throw e;
+	}
+	return fd;
+}
+
 export default function uploadPage(req: Request, res: Response, next: NextFunction) {
 	const token = req.params.token;
-	let offset = Number(req.headers['X-offset'] || '0');
+	let offset = Number(req.headers['x-offset']);
+	if (Number.isNaN(offset)) {
+		res.statusCode = 400;
+		res.send('Missing header: x-offset');
+	}
 	const file = getOrNull(store.getState(), 'pendingFile', token);
 	if (!file) {
 		res.statusCode = 404;
 		res.send('entity not found');
 		return;
 	}
-	const dir = dirname(file.outputFile);
-	let promiseChain = mkdir(dir, {
-		recursive: true
-	}).then(() => open(file.outputFile, 'w'));
+	const rootPromise = openFile(file.outputFile, offset);
+	let promiseChain = rootPromise;
+
 
 	req.on('data', (e: Buffer) => {
 		promiseChain = promiseChain.then((fd) => fd.write(e, 0, e.length, offset).then(r => {
 			if (r.bytesWritten !== e.length) {
-				throw new Error('Bytes read from htp stream do not match bytes written');
+				throw new Error('Bytes read from http stream do not match bytes written');
 			}
 			offset += r.bytesWritten;
 			return fd;
@@ -41,6 +61,11 @@ export default function uploadPage(req: Request, res: Response, next: NextFuncti
 					store.dispatch(crudDelete('pendingFile', file.token));
 				}
 			}
+		}).catch(e => {
+			rootPromise.then(fd => fd.close());
+			res.statusCode = 500;
+			console.error('Upload handler error', e);
+			res.send(e.toString());
 		});
 	});
 }
