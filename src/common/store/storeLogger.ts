1bc432a7b5e1994b5d5ec6a4da4ffece8fcd9c42
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2020-08-17 11:59:02 +0200

    Nicer store logger

--- src/common/store/storeLogger.ts
+++ src/common/store/storeLogger.ts
@@ -1,13 +1,21 @@
 import { Middleware } from 'redux';
+import colors from 'colors/safe';
 
 export default function storeLogger() {
 	const middleWare: Middleware = (api) => {
 		return (next)  => {
 			return (action) => {
-				const state = next(action);
-				console.log('ACTION: ' + JSON.stringify(state));
+				const newAction = next(action);
+				const { type, ...actionData } = newAction;
+				const dataAsString = JSON.stringify(actionData, (key, value) => {
+					if (value === null || typeof value !== 'object' || Object.getPrototypeOf(value) === Object.prototype || Object.getPrototypeOf(value) === Array.prototype) {
+						return value;
+					}
+					return `INSTANCE OF ${Object.getPrototypeOf(value)?.constructor.name}`;
+				});
+				console.log(`${colors.green('DISPATCH:')} ${colors.cyan(type)} ${colors.reset(dataAsString)}`);
 				//console.dir(api.getState());
-				return state;
+				return newAction;
 			};
 		};
 	};
